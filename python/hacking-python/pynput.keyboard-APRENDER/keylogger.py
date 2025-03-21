e46e7c18d8a0c7632cecebff245a6880c2618a83
marpehc <murciamarcos39@gmail.com>
2025-03-21 22:10:31 +0100

    update keylogger.py
    Quito toda la basura.

--- python/hacking-python/pynput.keyboard-APRENDER/keylogger.py
+++ python/hacking-python/pynput.keyboard-APRENDER/keylogger.py
@@ -1,8 +1,6 @@
 import pynput
-from pynput.keyboard import Controller
-import time
 
-keyboard = Controller
+
 
 def tecla_presionada(key):
     print(key)
@@ -11,6 +9,3 @@ keyboard_listener = pynput.keyboard.Listener(on_press=tecla_presionada)
 
 with keyboard_listener:
     keyboard_listener.join()
-
-time.sleep(3)
-keyboard.type("Hola, esto es un mensaje automático.")
